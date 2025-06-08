900224b8e3467b6b64f8c531d9e64a5fa2e99b2d
santoshsaranyan <saranyan.s@northeastern.edu>
2025-06-08 13:03:57 -0400

    Added code to check if table exists before creating

--- utilities/dbcreator.py
+++ utilities/dbcreator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 
 
 
@@ -26,43 +26,49 @@ def create_listings_table(engine) -> None:
     
     :param engine: SQLAlchemy engine object.
     """
+    table_name = "listings"
+    if table_exists(engine, table_name):
+        print(f"Table '{table_name}' exists.")
+        
+    else:
+        print(f"Table '{table_name}' does not exist.")
     
-    listingsSchemaQuery = """
-    CREATE TABLE IF NOT EXISTS listings (
-        listing_id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        description TEXT,
-        host_id INTEGER NOT NULL,
-        listing_url TEXT NOT NULL,
-        location_id INTEGER NOT NULL,
-        neighborhood_overview TEXT,
-        picture_url TEXT,
-        latitude FLOAT,
-        longitude FLOAT,
-        property_type TEXT,
-        room_type TEXT,
-        accommodates INTEGER,
-        bathrooms FLOAT,
-        bedrooms INTEGER,
-        beds INTEGER,
-        amenities TEXT,
-        license TEXT,
-        overall_rating FLOAT CHECK(overall_rating >= 0 AND overall_rating <= 5),
-        accuracy_rating FLOAT CHECK(accuracy_rating >= 0 AND accuracy_rating <= 5),
-        cleanliness_rating FLOAT CHECK(cleanliness_rating >= 0 AND cleanliness_rating <= 5),
-        checkin_rating FLOAT CHECK(checkin_rating >= 0 AND checkin_rating <= 5),
-        communication_rating FLOAT CHECK(communication_rating >= 0 AND communication_rating <= 5),
-        location_rating FLOAT CHECK(location_rating >= 0 AND location_rating <= 5),
-        value_rating FLOAT CHECK(value_rating >= 0 AND value_rating <= 5),
-        number_of_reviews INTEGER DEFAULT 0,
-        FOREIGN KEY (host_id) REFERENCES hosts(host_id),
-        FOREIGN KEY (location_id) REFERENCES locations(location_id)
-    );
-    """
-    
-    with engine.connect() as connection:
-        connection.execute(listingsSchemaQuery)
-        print("Listings table created successfully.")
+        listingsSchemaQuery = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            listing_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            description TEXT,
+            host_id INTEGER NOT NULL,
+            listing_url TEXT NOT NULL,
+            location_id INTEGER NOT NULL,
+            neighborhood_overview TEXT,
+            picture_url TEXT,
+            latitude FLOAT,
+            longitude FLOAT,
+            property_type TEXT,
+            room_type TEXT,
+            accommodates INTEGER,
+            bathrooms FLOAT,
+            bedrooms INTEGER,
+            beds INTEGER,
+            amenities TEXT,
+            license TEXT,
+            overall_rating FLOAT CHECK(overall_rating >= 0 AND overall_rating <= 5),
+            accuracy_rating FLOAT CHECK(accuracy_rating >= 0 AND accuracy_rating <= 5),
+            cleanliness_rating FLOAT CHECK(cleanliness_rating >= 0 AND cleanliness_rating <= 5),
+            checkin_rating FLOAT CHECK(checkin_rating >= 0 AND checkin_rating <= 5),
+            communication_rating FLOAT CHECK(communication_rating >= 0 AND communication_rating <= 5),
+            location_rating FLOAT CHECK(location_rating >= 0 AND location_rating <= 5),
+            value_rating FLOAT CHECK(value_rating >= 0 AND value_rating <= 5),
+            number_of_reviews INTEGER DEFAULT 0,
+            FOREIGN KEY (host_id) REFERENCES hosts(host_id),
+            FOREIGN KEY (location_id) REFERENCES locations(location_id)
+        );
+        """
+        
+        with engine.connect() as connection:
+            connection.execute(listingsSchemaQuery)
+            print("Listings table created successfully.")
 
     
 
@@ -72,22 +78,28 @@ def create_reviews_table(engine) -> None:
     
     :param engine: SQLAlchemy engine object.
     """
+    table_name = "reviews"
+    if table_exists(engine, table_name):
+        print(f"Table '{table_name}' exists.")
+        
+    else:
+        print(f"Table '{table_name}' does not exist.")
     
-    reviewsSchemaQuery = """
-    CREATE TABLE IF NOT EXISTS reviews (
-        review_id INTEGER PRIMARY KEY,
-        listing_id INTEGER NOT NULL,
-        date DATE NOT NULL,
-        reviewer_id TEXT NOT NULL,
-        reviewer_name TEXT NOT NULL,
-        comments TEXT,
-        FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
-    );
-    """
-    
-    with engine.connect() as connection:
-        connection.execute(reviewsSchemaQuery)
-        print("Reviews table created successfully.")
+        reviewsSchemaQuery = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            review_id INTEGER PRIMARY KEY,
+            listing_id INTEGER NOT NULL,
+            date DATE NOT NULL,
+            reviewer_id TEXT NOT NULL,
+            reviewer_name TEXT NOT NULL,
+            comments TEXT,
+            FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
+        );
+        """
+        
+        with engine.connect() as connection:
+            connection.execute(reviewsSchemaQuery)
+            print("Reviews table created successfully.")
     
 
 
@@ -97,32 +109,38 @@ def create_hosts_table(engine) -> None:
     
     :param engine: SQLAlchemy engine object.
     """
-    
-    hostsSchemaQuery = """
-    CREATE TABLE IF NOT EXISTS hosts (
-        host_id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        host_url TEXT NOT NULL,
-        host_since TEXT,
-        host_location_id TEXT,
-        host_about TEXT,
-        host_response_time TEXT,
-        host_response_rate FLOAT CHECK(host_response_rate >= 0 AND host_response_rate <= 100),
-        host_acceptance_rate FLOAT CHECK(host_acceptance_rate >= 0 AND host_acceptance_rate <= 100),
-        host_is_superhost BOOLEAN,
-        host_thumbnail_url TEXT,
-        host_picture_url TEXT,
-        host_listings_count INTEGER DEFAULT 0,
-        host_verifications TEXT,
-        host_has_profile_pic BOOLEAN,
-        host_identity_verified BOOLEAN,
-        FOREIGN KEY (host_location_id) REFERENCES locations(location_id)
-    );
-    """
-    
-    with engine.connect() as connection:
-        connection.execute(hostsSchemaQuery)
-        print("Hosts table created successfully.")
+    table_name = "hosts"
+    if table_exists(engine, table_name):
+        print(f"Table '{table_name}' exists.")
+        
+    else:
+        print(f"Table '{table_name}' does not exist.")
+        
+        hostsSchemaQuery = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            host_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            host_url TEXT NOT NULL,
+            host_since TEXT,
+            host_location_id TEXT,
+            host_about TEXT,
+            host_response_time TEXT,
+            host_response_rate FLOAT CHECK(host_response_rate >= 0 AND host_response_rate <= 100),
+            host_acceptance_rate FLOAT CHECK(host_acceptance_rate >= 0 AND host_acceptance_rate <= 100),
+            host_is_superhost BOOLEAN,
+            host_thumbnail_url TEXT,
+            host_picture_url TEXT,
+            host_listings_count INTEGER DEFAULT 0,
+            host_verifications TEXT,
+            host_has_profile_pic BOOLEAN,
+            host_identity_verified BOOLEAN,
+            FOREIGN KEY (host_location_id) REFERENCES locations(location_id)
+        );
+        """
+        
+        with engine.connect() as connection:
+            connection.execute(hostsSchemaQuery)
+            print("Hosts table created successfully.")
         
     
     
@@ -133,18 +151,24 @@ def create_locations_table(engine) -> None:
     :param engine: SQLAlchemy engine object.
     """
     
-    locationsSchemaQuery = """
-    CREATE TABLE IF NOT EXISTS locations (
-        location_id INTEGER PRIMARY KEY,
-        location TEXT NOT NULL,
-        neighborhood TEXT,
-        FOREIGN KEY (listing_id) REFERENCES listings(id)
-    );
-    """
-    
-    with engine.connect() as connection:
-        connection.execute(locationsSchemaQuery)
-        print("Locations table created successfully.")
+    table_name = "locations"
+    if table_exists(engine, table_name):
+        print(f"Table '{table_name}' exists.")
+        
+    else:
+        print(f"Table '{table_name}' does not exist.")
+        locationsSchemaQuery = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            location_id INTEGER PRIMARY KEY,
+            location TEXT NOT NULL,
+            neighborhood TEXT,
+            FOREIGN KEY (listing_id) REFERENCES listings(id)
+        );
+        """
+        
+        with engine.connect() as connection:
+            connection.execute(locationsSchemaQuery)
+            print("Locations table created successfully.")
     
     
 
@@ -155,21 +179,28 @@ def create_availability_table(engine) -> None:
     :param engine: SQLAlchemy engine object.
     """
     
-    availabilitySchemaQuery = """
-    CREATE TABLE IF NOT EXISTS availability (
-        availability_id INTEGER PRIMARY KEY,
-        listing_id INTEGER NOT NULL,
-        date DATE NOT NULL,
-        available BOOLEAN NOT NULL,
-        minimum_nights INTEGER DEFAULT 1,
-        maximum_nights INTEGER DEFAULT 365,
-        FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
-    );
-    """
+    table_name = "availability"
+    if table_exists(engine, table_name):
+        print(f"Table '{table_name}' exists.")
+        
+    else:
+        print(f"Table '{table_name}' does not exist.")
     
-    with engine.connect() as connection:
-        connection.execute(availabilitySchemaQuery)
-        print("Availability table created successfully.")
+        availabilitySchemaQuery = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            availability_id INTEGER PRIMARY KEY,
+            listing_id INTEGER NOT NULL,
+            date DATE NOT NULL,
+            available BOOLEAN NOT NULL,
+            minimum_nights INTEGER DEFAULT 1,
+            maximum_nights INTEGER DEFAULT 365,
+            FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
+        );
+        """
+        
+        with engine.connect() as connection:
+            connection.execute(availabilitySchemaQuery)
+            print("Availability table created successfully.")
         
         
 
@@ -227,6 +258,21 @@ def read_and_insert_date(engine) -> None:
         print("Availability data inserted successfully.")
     else:
         print("No data to insert into the database.")
+
+
+
+def table_exists(engine, table_name):
+    """Checks if a table exists in the database.
+
+    Args:
+        engine: SQLAlchemy engine object.
+        table_name: Name of the table to check.
+
+    Returns:
+        True if the table exists, False otherwise.
+    """
+    inspector = inspect(engine)
+    return inspector.has_table(table_name)
 
 
 
