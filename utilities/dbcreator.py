62bdb3eda8da84f0ad1578f356041ca7577f0e4c
santoshsaranyan <saranyan.s@northeastern.edu>
2025-06-08 18:09:48 -0400

    Improved code documentation and readability

--- utilities/dbcreator.py
+++ utilities/dbcreator.py
@@ -1,21 +1,46 @@
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine, inspect
+import logging
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+"""
+This script creates a MySQL database and its tables, reads data from CSV files, and inserts the data into the database.
+It uses SQLAlchemy for database operations and pandas for data manipulation.
+"""
+
+def main() -> None:
+    
+    # Creates a connection to a MySQL database.
+    mySQLEngine = create_engine('mysql+pymysql://user:password@localhost/db_name', echo=True)
+    
+    # Create the database tables.
+    create_all_tables(mySQLEngine)
+    
+    # Read data from CSV files and insert it into the database.
+    insert_data_to_table(mySQLEngine)
+    
+    
 
 def read_data(filePath: str) -> pd.DataFrame:
     """
     Reads data from a CSV file and returns it as a pandas DataFrame.
     
-    :param filePath: Path to the CSV file.
-    :return: DataFrame containing the data.
+    Parameters:
+        filePath: Path to the CSV file.
+        
+    Returns:
+        return: pandas DataFrame containing the data.
     """
+    
     try:
         data = pd.read_csv(filePath)
         return data
+    
     except Exception as e:
-        print(f"Error reading the file: {e}")
+        logging.info(f"Error reading the file: {e}")
         return pd.DataFrame() 
     
     
@@ -24,19 +49,22 @@ def create_listings_table(engine) -> None:
     """
     Creates the listings table in the database.
     
-    :param engine: SQLAlchemy engine object.
+    Parameters:
+        engine: SQLAlchemy engine object.
+    Returns:
+        None
     """
     
-    # Note: MySQL does not throw an error if the table already exists during creating with the IF NOT EXISTS clause.
-    # But, we still check for its existence anyway to avoid any unforseen bugs, as this will be used as part of an automated script.
-    
+    # Note: MySQL does not throw an error if the table already exists during table creation with the IF NOT EXISTS clause.
+    # But, we still check for its existence anyway to log what is happening and also to avoid any unforseen bugs, as this will be used as part of an automated script.
+     
     table_name = "listings"
     
     if table_exists(engine, table_name):
-        print(f"Table '{table_name}' exists.")
+        logging.info(f"Table '{table_name}' exists.")
         
     else:
-        print(f"Table '{table_name}' does not exist.")
+        logging.info(f"Table '{table_name}' does not exist.")
     
         listingsSchemaQuery = f"""
         CREATE TABLE IF NOT EXISTS {table_name} (
@@ -73,7 +101,7 @@ def create_listings_table(engine) -> None:
         
         with engine.connect() as connection:
             connection.execute(listingsSchemaQuery)
-            print("Listings table created successfully.")
+            logging.info("Listings table created successfully.")
 
     
 
@@ -81,19 +109,23 @@ def create_reviews_table(engine) -> None:
     """
     Creates the reviews table in the database.
     
-    :param engine: SQLAlchemy engine object.
+    Parameters:
+        engine: SQLAlchemy engine object.
+    
+    Returns:
+        None
     """
     
-    # Note: MySQL does not throw an error if the table already exists during creating with the IF NOT EXISTS clause.
-    # But, we still check for its existence anyway to avoid any unforseen bugs, as this will be used as part of an automated script.
-    
+    # Note: MySQL does not throw an error if the table already exists during table creation with the IF NOT EXISTS clause.
+    # But, we still check for its existence anyway to log what is happening and also to avoid any unforseen bugs, as this will be used as part of an automated script.
+   
     table_name = "reviews"
     
     if table_exists(engine, table_name):
-        print(f"Table '{table_name}' exists.")
+        logging.info(f"Table '{table_name}' exists.")
         
     else:
-        print(f"Table '{table_name}' does not exist.")
+        logging.info(f"Table '{table_name}' does not exist.")
     
         reviewsSchemaQuery = f"""
         CREATE TABLE IF NOT EXISTS {table_name} (
@@ -109,7 +141,7 @@ def create_reviews_table(engine) -> None:
         
         with engine.connect() as connection:
             connection.execute(reviewsSchemaQuery)
-            print("Reviews table created successfully.")
+            logging.info("Reviews table created successfully.")
     
 
 
@@ -117,19 +149,24 @@ def create_hosts_table(engine) -> None:
     """
     Creates the hosts table in the database.
     
-    :param engine: SQLAlchemy engine object.
+    Parameters:
+        engine: SQLAlchemy engine object.
+        
+    Returns:
+        None
     """
     
-    # Note: MySQL does not throw an error if the table already exists during creating with the IF NOT EXISTS clause.
-    # But, we still check for its existence anyway to avoid any unforseen bugs, as this will be used as part of an automated script.
+    # Note: MySQL does not throw an error if the table already exists during table creation with the IF NOT EXISTS clause.
+    # But, we still check for its existence anyway to log what is happening and also to avoid any unforseen bugs, as this will be used as part of an automated script.
+  
     
     table_name = "hosts"
     
     if table_exists(engine, table_name):
-        print(f"Table '{table_name}' exists.")
+        logging.info(f"Table '{table_name}' exists.")
         
     else:
-        print(f"Table '{table_name}' does not exist.")
+        logging.info(f"Table '{table_name}' does not exist.")
         
         hostsSchemaQuery = f"""
         CREATE TABLE IF NOT EXISTS {table_name} (
@@ -155,7 +192,7 @@ def create_hosts_table(engine) -> None:
         
         with engine.connect() as connection:
             connection.execute(hostsSchemaQuery)
-            print("Hosts table created successfully.")
+            logging.info("Hosts table created successfully.")
         
     
     
@@ -163,31 +200,34 @@ def create_locations_table(engine) -> None:
     """
     Creates the locations table in the database.
     
-    :param engine: SQLAlchemy engine object.
+    Parameters:
+        engine: SQLAlchemy engine object.
+        
+    Returns:
+        None
     """
     
-    # Note: MySQL does not throw an error if the table already exists during creating with the IF NOT EXISTS clause.
-    # But, we still check for its existence anyway to avoid any unforseen bugs, as this will be used as part of an automated script.
+    # Note: MySQL does not throw an error if the table already exists during table creation with the IF NOT EXISTS clause.
+    # But, we still check for its existence anyway to log what is happening and also to avoid any unforseen bugs, as this will be used as part of an automated script.
     
     table_name = "locations"
     
     if table_exists(engine, table_name):
-        print(f"Table '{table_name}' exists.")
+        logging.info(f"Table '{table_name}' exists.")
         
     else:
-        print(f"Table '{table_name}' does not exist.")
+        logging.info(f"Table '{table_name}' does not exist.")
         locationsSchemaQuery = f"""
         CREATE TABLE IF NOT EXISTS {table_name} (
             location_id INTEGER PRIMARY KEY,
             location TEXT NOT NULL,
-            neighborhood TEXT,
-            FOREIGN KEY (listing_id) REFERENCES listings(id)
+            neighborhood TEXT
         );
         """
         
         with engine.connect() as connection:
             connection.execute(locationsSchemaQuery)
-            print("Locations table created successfully.")
+            logging.info("Locations table created successfully.")
     
     
 
@@ -195,35 +235,39 @@ def create_availability_table(engine) -> None:
     """
     Creates the availability table in the database.
     
-    :param engine: SQLAlchemy engine object.
+    Parameters:
+        engine: SQLAlchemy engine object.
+        
+    Returns:
+        None
     """ 
     
-    # Note: MySQL does not throw an error if the table already exists during creating with the IF NOT EXISTS clause.
-    # But, we still check for its existence anyway to avoid any unforseen bugs, as this will be used as part of an automated script.
-    
+    # Note: MySQL does not throw an error if the table already exists during table creation with the IF NOT EXISTS clause.
+    # But, we still check for its existence anyway to log what is happening and also to avoid any unforseen bugs, as this will be used as part of an automated script.
+   
     table_name = "availability"
     
     if table_exists(engine, table_name):
-        print(f"Table '{table_name}' exists.")
+        logging.info(f"Table '{table_name}' exists.")
         
     else:
-        print(f"Table '{table_name}' does not exist.")
+        logging.info(f"Table '{table_name}' does not exist.")
     
         availabilitySchemaQuery = f"""
         CREATE TABLE IF NOT EXISTS {table_name} (
-            availability_id INTEGER PRIMARY KEY,
             listing_id INTEGER NOT NULL,
             date DATE NOT NULL,
             available BOOLEAN NOT NULL,
             minimum_nights INTEGER DEFAULT 1,
             maximum_nights INTEGER DEFAULT 365,
+            PRIMARY KEY (listing_id, date),
             FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
         );
         """
         
         with engine.connect() as connection:
             connection.execute(availabilitySchemaQuery)
-            print("Availability table created successfully.")
+            logging.info("Availability table created successfully.")
         
         
 
@@ -231,63 +275,73 @@ def create_all_tables(engine) -> None:
     """
     Creates all tables in the database.
     
-    :param engine: SQLAlchemy engine object.
+    Parameters:
+        engine: SQLAlchemy engine object.
+        
+    Returns:
+        None
     """
-    print("Creating all tables in the database...")
+    
+    logging.info("Creating all tables in the database...")
     
     # Create all tables
+    create_locations_table(engine)
+    create_hosts_table(engine)
     create_listings_table(engine)
     create_reviews_table(engine)
-    create_hosts_table(engine)
-    create_locations_table(engine)
     create_availability_table(engine)
-    print("All tables created successfully.")
+    
+    logging.info("All tables created successfully.")
     
     
     
-def read_and_insert_date(engine) -> None:
+def insert_data_to_table(engine) -> None:
     """
     Reads data from CSV files and inserts it into the database.
     
-    :param engine: SQLAlchemy engine object.
+    Parameters:
+        engine: SQLAlchemy engine object.
+        
+    Returns:
+        None
     """
     
     # Read data from CSV files
-    print("Reading data from CSV files...")
+    logging.info("Reading data from CSV files...")
     listingsData = read_data('cleaned_listings.csv')
     reviewsData = read_data('cleaned_reviews.csv')
     hostsData = read_data('cleaned_hosts.csv')
     locationsData = read_data('cleaned_locations.csv')
     availabilityData = read_data('cleaned_availability.csv')
-    print("Data read successfully.")
+    logging.info("Data read successfully.")
     
     
     # Insert data into the database tables
-    print("Inserting data into the database...")
+    logging.info("Inserting data into the database...")
     if not listingsData.empty:
         listingsData.to_sql('listings', con=engine, if_exists='append', index=False)
-        print("Listings data inserted successfully.")
+        logging.info("Listings data inserted successfully.")
     if not reviewsData.empty:
         reviewsData.to_sql('reviews', con=engine, if_exists='append', index=False)
-        print("Reviews data inserted successfully.")
+        logging.info("Reviews data inserted successfully.")
     if not hostsData.empty:
         hostsData.to_sql('hosts', con=engine, if_exists='append', index=False)
-        print("Hosts data inserted successfully.")
+        logging.info("Hosts data inserted successfully.")
     if not locationsData.empty:
         locationsData.to_sql('locations', con=engine, if_exists='append', index=False)
-        print("Locations data inserted successfully.")
+        logging.info("Locations data inserted successfully.")
     if not availabilityData.empty:
         availabilityData.to_sql('availability', con=engine, if_exists='append', index=False)
-        print("Availability data inserted successfully.")
+        logging.info("Availability data inserted successfully.")
     else:
-        print("No data to insert into the database.")
+        logging.info("No data to insert into the database.")
 
 
 
-def table_exists(engine, table_name):
+def table_exists(engine, table_name: str) -> bool:
     """Checks if a table exists in the database.
 
-    Args:
+    Parameters:
         engine: SQLAlchemy engine object.
         table_name: Name of the table to check.
 
@@ -296,19 +350,6 @@ def table_exists(engine, table_name):
     """
     inspector = inspect(engine)
     return inspector.has_table(table_name)
-
-
-
-def main() -> None:
-    
-    # Creates a connection to a MySQL database.
-    mySQLEngine = create_engine('mysql+pymysql://user:password@localhost/db_name', echo=True)
-    
-    # Create the database tables.
-    create_all_tables(mySQLEngine)
-    
-    # Read data from CSV files and insert it into the database.
-    read_and_insert_date(mySQLEngine)
     
 
 
