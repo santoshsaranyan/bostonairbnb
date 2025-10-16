a38a0e93440f2d6cd54b85ae7ed7691f3d1fd511
santoshsaranyan <saranyan.s@northeastern.edu>
2025-10-16 17:17:51 -0400

    Documentation update

--- streamlit_app/App_Home_Page.py
+++ streamlit_app/App_Home_Page.py
@@ -4,15 +4,15 @@ st.set_page_config(page_title="Boston BnB Insights App", layout="wide", page_ico
 
 st.title("🏘️ Welcome to the Boston BnB Insights App!")
 
+
 with st.sidebar:
    st.image("https://t3.ftcdn.net/jpg/05/89/24/50/360_F_589245011_2eyvpGgTRGZT3Hw4ScUj9QPwvOLp3XsQ.jpg", width=150)
    st.divider()
 
    st.header("How to Use")
    st.markdown("""
-   1. Run each pipeline step in order  
-   2. View processed data and analytics  
-   3. Re-run steps anytime to refresh data
+   1. Verify each pipeline step in order
+   2. View processed insights and analytics 
    """)
    
    st.markdown("View the source code on [GitHub](https://github.com/santoshsaranyan/bostonairbnb)")
@@ -27,15 +27,24 @@ st.markdown(" This Streamlit app demonstrates a complete end-to-end **data engin
 
 st.info("💡 *Navigate to the various pages using the top-left section of the sidebar* ")
 
-st.subheader("🧱 Pipeline Overview")
+st.subheader("🏛️ Data Architecture")
+st.markdown("""The architecture of the end to end pipeline is illustrated below:""")
+try:
+   st.image("images/DataArchitecture.png", caption="Data Architecture")
+except:
+   st.image("streamlit_app/images/DataArchitecture.png", caption="Data Architecture")
+
+st.subheader("🧱 ETL Pipeline Overview")
 
 st.markdown("""
+The whole ETL process is contained within a **Docker** container, and automated to run on a monthly schedule using **GitHub Actions**.
+The pipeline consists of three main steps:
 1. **Data Scraping**  
    Retrieves the latest Boston Airbnb datasets directly from [InsideAirbnb](https://insideairbnb.com), which provides data for the past 12 months updated quarterly.
 2. **Data Preprocessing**  
    Cleans, standardizes, and prepares the data for storage and analysis — handling missing values, formatting columns, and optimizing the dataset for database loading.
 3. **Database Loading**  
-   Loads the processed data into a **PostgreSQL** database (running inside a **Docker container**) for efficient querying and analytics.
+   Loads the raw and processed data into a **PostgreSQL** database (hosted by **Supabase**) for efficient querying and analytics. Also refreshes the materialized views to ensure up-to-date insights.
 """)
 
 st.subheader("📊 Analysis and Visualization")
